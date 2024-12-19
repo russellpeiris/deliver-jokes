9aef96b4612c65e21bd9a1535f2ab89a8c724d54
Russell <ardpeiris@gmail.com>
2024-12-20 04:08:09 +0530

    update main.ts to listen on all network interfaces

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
   app.setGlobalPrefix('deliver-service');
   app.enableCors();
-  await app.listen(process.env.PORT ?? 4003);
+  await app.listen(process.env.PORT ?? 4003, '0.0.0.0');
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();

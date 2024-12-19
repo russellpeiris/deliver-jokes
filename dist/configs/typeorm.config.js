98384513fb69c06cd45017288302912f6dc4a394
Russell <ardpeiris@gmail.com>
2024-12-20 05:00:24 +0530

    add environment configuration files and Docker support; update TypeORM config to use environment variables

--- dist/configs/typeorm.config.js
+++ dist/configs/typeorm.config.js
@@ -1,14 +1,18 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.typeOrmConfig = void 0;
+const dotenv = require("dotenv");
+const environment = process.env.NODE_ENV || 'development';
+console.log('environment :', environment);
+dotenv.config({ path: `.env.${environment}` });
 exports.typeOrmConfig = {
     type: 'mysql',
-    host: 'localhost',
-    port: 3306,
-    username: 'root',
-    password: 'admin',
-    database: 'jokesdb',
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT),
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
     entities: [__dirname + '/../**/*.entity.{js,ts}'],
-    synchronize: true,
+    synchronize: environment === 'development',
 };
 //# sourceMappingURL=typeorm.config.js.map
--- src/configs/typeorm.config.ts
+++ src/configs/typeorm.config.ts
@@ -1,12 +1,17 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import * as dotenv from 'dotenv';
+
+const environment = process.env.NODE_ENV || 'development';
+console.log('environment :', environment);
+dotenv.config({ path: `.env.${environment}` });
 
 export const typeOrmConfig: TypeOrmModuleOptions = {
   type: 'mysql',
-  host: 'localhost',
-  port: 3306,
-  username: 'root',
-  password: 'admin',
-  database: 'jokesdb',
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   entities: [__dirname + '/../**/*.entity.{js,ts}'],
-  synchronize: true,
+  synchronize: environment === 'development',
 };
